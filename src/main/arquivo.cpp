ab8bc69341aa9919920b317324535f2e6b810eb0
caiosdeo <caiosdeo@hotmail.com>
2019-09-01 20:05:47 -0300

    Funçao de leitura do arquivo de entrada implementada

--- src/main/arquivo.cpp
+++ src/main/arquivo.cpp
@@ -1,7 +1,25 @@
 #include "arquivo.h"
 #include "review.h"
 
-void Arquivo::leitura(fstream &arquivo){
+Review** leitura(fstream &entrada, fstream &dataset){
 
+    int qtdConjuntos;
+    int n;
 
+    entrada >> qtdConjuntos;
+
+    Review** conjuntos = new Review*[qtdConjuntos];
+
+    for(int i = 0; i < qtdConjuntos; i++){
+        
+        entrada >> n;
+
+        conjuntos[i] = new Review[n];
+
+        conjuntos[i] = conjuntos[i]->leituraDados(dataset, n);
+
+    }
+
+    return conjuntos;
+    
 }
