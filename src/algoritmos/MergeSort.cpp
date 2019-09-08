318eaf83ae1c99c19ca1bf7b6e47a14a7af3aba9
caiosdeo <caiosdeo@hotmail.com>
2019-09-07 21:57:58 -0300

    mergeSort consertado

--- src/algoritmos/MergeSort.cpp
+++ src/algoritmos/MergeSort.cpp
@@ -4,63 +4,124 @@
 
 using namespace std;
 
-/// implementacao do Merge Sort
+void mergeInteiros(int *vet, int esq, int med, int dir){
 
-/// merge para vetor de inteiros
-void MergeSort::mergeV(int *vet, int esq, int med, int dir)
-{
     int i, j, k;
-    int tamE = med - esq +1, tamD = dir - med;
-    int es[tamE], di[tamD];
+    int tamE = med - esq + 1;
+    int tamD = dir - med;
 
-    for(i = 0; i < tamE; i++) es[i] = vet[esq+i];
-    for(j = 0; j < tamD; j++) di[j] = vet[med+1+j];
+    //Vetores temporários
+    int E[tamE], D[tamD];
 
-    for(i=0, j=0, k = esq; i < tamE && j < tamD; k++)
-    {
-        if(es[i] <= di[j]){vet[k] = es[i]; i++;}
-        else {vet[k] = di[j]; j++;}
+    //Copiando dados para os vetores temporários
+    for(i = 0; i < tamE; i++) 
+        E[i] = vet[esq+i];
+    for(j = 0; j < tamD; j++)
+        D[j] = vet[med+1+j];
+
+    // Intercalando os vetores temporários
+    for(i = 0, j = 0, k = esq; i < tamE && j < tamD; k++){
+
+        if(E[i] <= D[j]){
+        
+            vet[k] = E[i];
+            i++;
+        
+        }else{
+        
+            vet[k] = D[j]; 
+            j++;
+        
+        }
+
     }
+
+    //Copiando os remanescentes de E
+    while (i < tamE){ 
+        vet[k] = E[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    //Copiando os remanescentes de D
+    while (j < tamD){ 
+        vet[k] = D[j]; 
+        j++; 
+        k++; 
+    }
+
 }
 
-/// Merge para vetor Review
-void MergeSort::mergeR(Review *r, int esq, int med, int dir, int *numComp, int *numReg)
-{
-    int i,j,k = esq;
-    int sizeE = med - esq +1, sizeD = dir - med;
+void mergeReviews(Review *r, int esq, int med, int dir, int *numComp, int *numReg){
 
-    Review *es, *di;
-    es = r->leituraDados(NULL, sizeE);
-    di = r->leituraDados(NULL, sizeD);
+    int i, j, k = esq;
+    int tamE = med - esq + 1;
+    int tamD = dir - med;
 
-    for(i = 0; i < sizeE; i++) {es[i].setNewInfo(r[esq+i].getUser(), r[esq+i].getRating(), r[esq+i].getId()); *numReg+=1;}
-    for(i = 0; j < sizeD; i++) {di[j].setNewInfo(r[med+1+j].getUser(), r[med+1+j].getRating(), r[med+1+j].getId()); *numReg+=1;}
+    Review E[tamE], D[tamD];
 
-    for(i = 0, j = 0, k = esq; i < sizeE && j < sizeD; k++)
-    {
-        if(es[i].getId() <= di[j].getId()){r[k].setNewInfo(es[i].getUser(), es[i].getRating(), es[i].getId()); i++;}
-        else {r[k].setNewInfo(di[j].getUser(), di[j].getRating(), di[j].getId()); j++;}
-        *numComp+=1;
+    for(i = 0; i < tamE; i++){
+        E[i] = r[esq + i];
     }
+    for(j = 0; j < tamD; j++){
+        D[j] = r[med + j + 1];
+    }
+
+    for(i = 0, j = 0, k = esq; i < tamE && j < tamD; k++){
+
+        (*numComp)++;
+
+        if(E[i].getId() <= D[j].getId()){
+            r[k] = E[i];
+            i++;
+        
+        }else{
+
+            r[k] = D[j];
+            j++;
+        }
+        
+    }
+
+    //Copiando os remanescentes de E
+    while (i < tamE){ 
+        r[k] = E[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    //Copiando os remanescentes de D
+    while (j < tamD){ 
+        r[k] = D[j]; 
+        j++; 
+        k++; 
+    }
+
 }
 
-void MergeSort::mergeSort(int *vet, Review *r, int esqV, int dirV, int esqR, int dirR, int *numComp, int *numReg)
-{
-    if(esqV >= 0 && dirV >= 0)
-    {
-        int medV = esqV + (dirV-esqV)/2;
-        mergeSort(vet, r, esqV, medV, -1, -1, numComp, numReg);
-        mergeSort(vet, r, medV+1, dirV, -1, -1, numComp, numReg);
+void mergeSortInteiros(int *vet, int esq, int dir, int *numComp, int *numReg){
 
-        mergeV(vet, esqV, medV, dirV);
+    if(esq < dir){
+
+        int med = (esq + dir)/2;
+
+        mergeSortInteiros(vet, esq, med, numComp, numReg);
+        mergeSortInteiros(vet, med+1, dir, numComp, numReg);
+
+        mergeInteiros(vet, esq, med, dir, numComp, numReg);
     }
 
-    if(esqR >= 0 && dirR >= 0)
-    {
-        int medR = esqR + (dirR-esqR)/2;
-        mergeSort(vet, r, -1, -1, esqR, medR, numComp, numReg);
-        mergeSort(vet, r, -1, -1, medR+1, dirR, numComp, numReg);
+}
 
-        mergeR(r, esqR, medR, dirR, numComp, numReg);
+void mergeSortReviews(Review *r, int esq, int dir, int *numComp, int *numReg){
+    
+    if(esq < dir){
+
+        int med = (esq + dir)/2;
+
+        mergeSortReviews(r, esq, med, numComp, numReg);
+        mergeSortReviews(r, med+1, dir, numComp, numReg);
+
+        mergeReviews(r, esq, med, dir, numComp, numReg);
     }
 }
--- src/algoritmos/MergeSort.h
+++ src/algoritmos/MergeSort.h
@@ -3,18 +3,52 @@
 #include "../classes/review.h"
 #include <fstream>
 
-using namespace std;
+/**
+ * mergeSort para vetor de inteiros
+ * @author caiosdeo
+ * @param vet Vetor de inteiros
+ * @param esq Começo do vetor
+ * @param dir Fim do vetor
+ * @param numComp número de comparações
+ * @param numReg número de registros
+ * @return void
+ */
+void mergeSortInteiros(int *vet, int esq, int dir, int *numComp, int *numReg);
 
-class MergeSort
-{
-    public:
-        MergeSort(){};
-        ~MergeSort(){};
-        void mergeSort(int *vet, Review *r, int esqV, int dirV, int esqR, int dirR, int *numComp, int *numReg);
+/**
+ * mergeSort para vetor de reviews
+ * @author caiosdeo
+ * @param vet Vetor de reviews
+ * @param esq Começo do vetor
+ * @param dir Fim do vetor
+ * @param numComp número de comparações
+ * @param numReg número de registros
+ * @return void
+ */
+void mergeSortReviews(Review *r, int esq, int dir, int *numComp, int *numReg);
 
-    private:
-        void mergeV(int *vet, int esq, int med, int dir);
-        void mergeR(Review *r, int esq, int med, int dir, int *numComp, int *numReg);
-};
+/**
+ * @author caiosdeo
+ * @param vet Vetor de inteiros
+ * @param esq início da vetor
+ * @param med meio do vetor
+ * @param dir fim da vetor
+ * @param numComparacoes número de comparações
+ * @param numTrocas número de trocas
+ * @return void
+ */
+void mergeInteiros(int *vet, int esq, int med, int dir, int *numComp, int *numReg);
+
+/**
+ * @author caiosdeo
+ * @param vet Vetor de reviews
+ * @param esq início da vetor
+ * @param med meio do vetor
+ * @param dir fim da vetor
+ * @param numComparacoes número de comparações
+ * @param numTrocas número de trocas
+ * @return void
+ */
+void mergeReviews(Review *r, int esq, int med, int dir, int *numComp, int *numReg);
 
 #endif // MERGESORT_H
