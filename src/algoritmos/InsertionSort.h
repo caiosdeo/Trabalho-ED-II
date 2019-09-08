03e5ef6477ae3dc6152166ea3fb41b9975f35318
caiosdeo <caiosdeo@hotmail.com>
2019-09-07 21:25:37 -0300

    insertionSort consertado

--- src/algoritmos/InsertionSort.h
+++ src/algoritmos/InsertionSort.h
@@ -1,17 +1,29 @@
 #ifndef INSERTIONSORT_H
 #define INSERTIONSORT_H
 #include "../classes/review.h"
-#include <fstream>
 
-using namespace std;
+/**
+ * insertionSort para vetor de inteiros
+ * @author caiosdeo
+ * @param vet Vetor de inteiros
+ * @param size Tamanho do vetor
+ * @param numComparacoes número de comparações
+ * @param numReg número de registros
+ * @return void
+ */
+void insertionSortInteiros(int *vet, int size, int *numComp, int *numReg);
 
-class InsertionSort
-{
-    public:
-        InsertionSort(){};
-        ~InsertionSort(){};
-        void insertionSort(int *vet, Review *r, int sizev, int sizer, int *numComp, int *numReg);
+/**
+ * insertionSort para vetor de reviews
+ * @author caiosdeo
+ * @param vet Vetor de reviews
+ * @param size Tamanho do vetor
+ * @param numComparacoes número de comparações
+ * @param numReg número de registros
+ * @return void
+ */
+void insertionSortReviews(Review *r, int size, int *numComp, int *numReg);
 
-};
+
 
 #endif // INSERTIONSORT_H
