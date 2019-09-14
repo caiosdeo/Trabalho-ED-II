b440c0d3f719cbc6e78eebf45465288ae9ef3606
caiosdeo <caiosdeo@hotmail.com>
2019-09-14 19:34:14 -0300

    gerarConjuntoChaves adicionada em auxiliaresHash.h

--- src/algoritmos/hash/auxiliaresHash.h
+++ src/algoritmos/hash/auxiliaresHash.h
@@ -1,6 +1,5 @@
 #include "../classes/review.h"
 
-// Retorna o valor de uma string 
 /**
  * Valor correspondente de string em inteiro
  *
@@ -9,3 +8,14 @@
  * @return valorString correspondente inteiro da soma dos caracteres da string.
  */
 int converterStringInteiro(string str);
+ 
+/**
+ * Função para gerar um conjunto de chaves para hashing 
+ * a partir de um conjunto de reviews
+ *
+ * @author eliascassis
+ * @param review Conjunto de reviews
+ * @param n Tamanho do conjunto de reviews
+ * @return chaves Conjunto de inteiros com as chaves
+ */
+int* gerarConjuntoChaves(Review* review, int n);
