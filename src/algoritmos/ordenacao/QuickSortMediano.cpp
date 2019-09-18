1126c3f0682130129a849608f37747bf56f5b3af
Developer-Hannya <42482469+Developer-Hannya@users.noreply.github.com>
2019-09-18 15:25:33 -0300

    mudei a parte da atualização...
    mudei a parte da atualização dos contadores de trocas e comparações

--- src/algoritmos/ordenacao/QuickSortMediano.cpp
+++ src/algoritmos/ordenacao/QuickSortMediano.cpp
@@ -18,14 +18,14 @@ int particaoId(int *vetor, int inicio, int fim, int *trocas, int *comparacoes) {
     int meio = (inicio + fim) / 2;
     int idI = vetor[inicio], idM = vetor[meio], idF = vetor[fim];
     int mediana = 0;
-    comparacoes++;
+    (*comparacoes)++;
     if (idI < idM) {
-        comparacoes++;
+        (*comparacoes)++;
         if (idM < idF) {
             mediana = meio;
         }
         else {
-            comparacoes++;
+            (*comparacoes)++;
             if (idI < idF) {
                 mediana = fim;
             }
@@ -35,12 +35,12 @@ int particaoId(int *vetor, int inicio, int fim, int *trocas, int *comparacoes) {
         }
     }
     else {
-        comparacoes++;;
+        (*comparacoes)++;
         if (idF < idM) {
             mediana = meio;
         }
         else {
-            comparacoes++;
+            (*comparacoes)++;
             if (idF < idI) {
                 mediana = fim;
             }
@@ -49,25 +49,25 @@ int particaoId(int *vetor, int inicio, int fim, int *trocas, int *comparacoes) {
             }
         }
     }
-    trocas++;
+    (*trocas)++;
     trocarId(vetor, mediana, fim);
     int pivo = vetor[fim];
     int i = inicio - 1;
     for (int j = 0; j < fim - 1; j++) {
-        comparacoes++;
+        (*comparacoes)++;
         if (vetor[j] <= pivo) {
             i++;
-            trocas++;
+            (*trocas)++;
             trocarId(vetor, i, j);
         }
     }
-    trocas++;
+    (*trocas)++;
     trocarId(vetor, i + 1, fim);
     return i + 1;
 }
 
 void quickSortMedianoId(int *vetor, int inicio, int fim, int *trocas, int *comparacoes) {
-comparacoes++;
+(*comparacoes)++;
 if (inicio < fim) {
         int q = particaoId(vetor, inicio, fim, trocas, comparacoes);
         quickSortMedianoId(vetor, inicio, q - 1, trocas, comparacoes);
@@ -86,14 +86,14 @@ int particaoReview(Review* r, int inicio, int fim, int *trocas, int *comparacoes
     int meio = r[(inicio + fim) / 2)].getId();
     int reviewI = r[inicio].getId(), reviewM = r[meio].getId(), reviewF = r[fim].getId();
     int mediana = 0;
-    comparacoes++;
+    (*comparacoes)++;
     if (reviewI < reviewM) {
-        comparacoes++;
+        (*comparacoes)++;
         if (reviewM < reviewF) {
             mediana = meio;
         }
         else {
-            comparacoes++;
+            (*comparacoes)++;
             if (reviewI < reviewF) {
                 mediana = fim;
             }
@@ -103,12 +103,12 @@ int particaoReview(Review* r, int inicio, int fim, int *trocas, int *comparacoes
         }
     }
     else {
-        comparacoes++;;
+        (*comparacoes)++;
         if (reviewF < reviewM) {
             mediana = meio;
         }
         else {
-            comparacoes++;
+            (*comparacoes)++;
             if (reviewF < reviewI) {
                 mediana = fim;
             }
@@ -117,19 +117,20 @@ int particaoReview(Review* r, int inicio, int fim, int *trocas, int *comparacoes
             }
         }
     }
-    trocas++;
+    //(*comparacoes)++;
+    (*trocas)++;
     trocarReview(r, mediana, fim);
     int pivo = r[fim].getId();
     int i = inicio - 1;
     for (int j = 0; j < fim - 1; j++) {
-        comparacoes++;
+        (*comparacoes)++;
         if (r[j].getId() <= pivo) {
             i++;
-            trocas++;
+            (*trocas)++;
             trocarReview(r, i, j);
         }
     }
-    trocas++;
+    (*trocas)++;
     trocarReview(r, i + 1, fim);
     return i + 1;
 }
