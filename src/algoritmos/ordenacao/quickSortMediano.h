c584f9294692d432f537623f2280e8cad97328e8
caiosdeo <caiosdeo@hotmail.com>
2019-09-25 15:21:19 -0300

    Removendo quicksort com mediana de Reviews

--- src/algoritmos/ordenacao/quickSortMediano.h
+++ src/algoritmos/ordenacao/quickSortMediano.h
@@ -6,11 +6,4 @@ int particaoId(int *vetor, int inicio, int fim, int *trocas, int *comparacoes);
 void trocarId(int *vetor, int a, int b);
 void quickSortMedianoId(int *vetor, int inicio, int fim, int *trocas, int *comparacoes);
 
-int particaoReview(Review* r, int inicio, int fim, int *trocas, int *comparacoes);
-void trocarReview(Review* r, int a, int b);
-void quickSortMedianoReview(Review* r, int inicio, int fim, int *trocas, int *comparacoes);
-
-
-
-
 #endif // QUICKSORTMEDIANO_H
