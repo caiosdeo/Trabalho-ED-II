c7b80ca6a1e7162389cb4686f97908386e997049
caiosdeo <caiosdeo@hotmail.com>
2019-09-26 17:22:47 -0300

    Corrigindo heapSort

--- src/algoritmos/ordenacao/HeapSort.cpp
+++ src/algoritmos/ordenacao/HeapSort.cpp
@@ -1,45 +1,45 @@
 #include "HeapSort.h"
-
-void trocarId (int a, int b) {
-    int aux = a;
-    a = b;
-    b = aux;
-}
+#include "quickSortRecursivo.h"
 
 void heapify(int *id, int tam, int indice, int *trocas, int *comparacoes) {
-            int maior = indice;
-            int esq = 2*indice + 1;
-            int dir = 2*indice + 2;
+    
+    int maior = indice;
+    int esq = 2*indice + 1;
+    int dir = 2*indice + 2;
 
-            (*comparacoes)+=2;
-            if (esq < tam && id[esq] > id[maior]) {
-                maior = esq;
-            }
+    (*comparacoes)+=2;
+    if (esq < tam && id[esq] > id[maior]) {
+        maior = esq;
+    }
 
-            (*comparacoes)+=2;
-            if (dir < tam && id[dir] > id[maior]) {
-                maior = dir;
-            }
+    (*comparacoes)+=2;
+    if (dir < tam && id[dir] > id[maior]) {
+        maior = dir;
+    }
 
-            if (maior != indice) {
-                trocarId (id[indice], id[maior]);
-                (*trocas)++;
-                heapify(id, tam, maior, trocas, comparacoes);
-            }
+    if (maior != indice) {
 
-        }
+        swap(id, indice, maior);
+        (*trocas)++;
+        heapify(id, tam, maior, trocas, comparacoes);
+    }
+
+}
+
 void heapSort(int *id, int tam, int *trocas, int *comparacoes) {
     // Build heap (rearrange array)
-    for (int i = tam / 2 - 1; i >= 0; i--)
+    for (int i = tam / 2; i >= 1; i--)
         heapify(id, tam, i, trocas, comparacoes);
 
     // One by one extract an element from heap
-    for (int i=tam-1; i>=0; i--)
-    {
+    for (int i = tam/2; i >= 2; i--){
+
         // Move current root to end
-        trocarId(id[0], id[i]);
+        swap(id, 1, i);
+        (*trocas)++;
 
         // call max heapify on the reduced heap
-        heapify(id, tam, i, trocas, comparacoes);
+        heapify(id, tam - 1, 1, trocas, comparacoes);
     }
+
 }
--- src/algoritmos/ordenacao/HeapSort.h
+++ src/algoritmos/ordenacao/HeapSort.h
@@ -1,17 +1,7 @@
 #ifndef HEAPSORT_H
 #define HEAPSORT_H
 
-class HeapSort
-{
-    public:
-        void heapify(int *id, int tam, int indice, int *trocas, int *comparacoes);
-        void trocarId(int a, int b);
-        void heapSort(int *id, int tam, int *trocas, int *comparacoes);
-
-
-    protected:
-
-    private:
-};
+void heapify(int *id, int tam, int indice, int *trocas, int *comparacoes);
+void heapSort(int *id, int tam, int *trocas, int *comparacoes);
 
 #endif // HEAPSORT_H
