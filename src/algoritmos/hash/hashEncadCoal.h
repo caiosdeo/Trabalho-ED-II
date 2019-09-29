ff1ecc53d7ce17e8cf429a7dd70cf75f53bec6fb
AndreCasarim <andrleite@ymail.com>
2019-09-29 10:06:50 -0300

    erros corrigidos

--- src/algoritmos/hash/hashEncadCoal.h
+++ src/algoritmos/hash/hashEncadCoal.h
@@ -1,9 +1,6 @@
 #ifndef HASHENCADCOAL_H
 #define HASHENCADCOAL_H
-<<<<<<< HEAD
 #include <stdio.h>
-=======
->>>>>>> e1444e77b917beba6cad8571b18fca014b0edc21
 #include "noHashCoal.h"
 
 using namespace std;
