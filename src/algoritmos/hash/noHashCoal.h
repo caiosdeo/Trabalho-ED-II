1bc077184476317a76ebfc9a4790e6493344b714
AndreCasarim <andrleite@ymail.com>
2019-09-28 22:51:36 -0300

    No para hash coalescido implementado

---
+++ src/algoritmos/hash/noHashCoal.h
@@ -0,0 +1,31 @@
+#ifndef NOHASHCOAL_H
+#define NOHASHCOAL_H
+
+
+class noHashCoal
+{
+    public:
+        noHashCoal(){};
+        ~noHashCoal(){};
+
+        int getInfo(){return this->chave;};
+        int getIndice(){return this->indice;};
+        noHashCoal* getProx(){return this->next;};
+        noHashCoal* getPrev(){return this->prev};
+        noHashCoal* getColisao() {return this->proxColisao;};
+
+        void setProx(HashPoint *p){this->next = p;};
+        void setPrev(HashPoint *p){this->prev = p;};
+        void setInfo(int chave){this->chave = chave;};
+        void setIndice(int indice){this->indice = indice;};
+        void setColisao(noHashCoal *p){this->colisao = p;};
+
+    private:
+        int chave; //inteiro contendo a informacao
+        int indice; //inteiro que armazena o indice
+        noHashCoal *next; //ponteiro para proxima posicao
+        noHashCoal *prev; //ponteiro para posicao anterior
+        noHashCoal *colisao; //posicao do elemento que colidiu
+};
+
+#endif // NOHASHCOAL_H
