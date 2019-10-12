5200d396c7fa2f944f4aae0a416730e318cbc81f
Elias Cyrino <eliascyrino@gmail.com>
2019-10-12 12:01:14 -0300

    algumas alterações sem sucesso no insertion sort

--- src/algoritmos/ordenacao/InsertionSort.cpp
+++ src/algoritmos/ordenacao/InsertionSort.cpp
@@ -6,27 +6,23 @@ using namespace std;
 /// Funcao Insertion Sort para vetor de unsigned de tamanho size
 void insertionSort(unsigned *vet, unsigned size, unsigned long long *numComp, unsigned long long *numReg)
 {
-    unsigned i, chave, j;
-    for(i = 1; i < size; i++){
+    unsigned chave, j;
+    for(unsigned i = 1; i < size; i++){
 
         chave = vet[i]; //copiando elemento i
-        j = i-1;
-
-        (*numReg)++; //atualiza numero de copias de registro
-        (*numComp)++; //atualiza numero de comparacoes
 
         // copiando elementos de j para j+1
-        while(j >= 0 && vet[j] > chave){
-
-            vet[j+1] = vet[j];
-            j--;
-            (*numComp)+=2;
+        for(j = i-1; j >= 0 && vet[j] > chave; j--){
+            
             (*numReg)++;
+            (*numComp)+=2;
+            vet[j+1] = vet[j];
 
         }
 
         vet[j+1] = chave; // copia elemento i a j+1
         (*numReg)++;
+
     }
 
 }
