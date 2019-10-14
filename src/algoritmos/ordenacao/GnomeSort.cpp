63195fe9b71fa655035b36efc222cb3ce57a506b
Elias Cyrino <eliascyrino@gmail.com>
2019-10-14 18:59:08 -0300

    gnomeSort documentado

--- src/algoritmos/ordenacao/GnomeSort.cpp
+++ src/algoritmos/ordenacao/GnomeSort.cpp
@@ -5,18 +5,21 @@ void gnomeSort(unsigned *v, unsigned n, unsigned long long *numComparacoes, unsi
 
     unsigned id = 0; 
   
+    // Para do laço - vetor inteiramente percorrido  
     while(id < n){ 
 
         if (id == 0) 
             id++; 
 
         (*numComparacoes)++;
+        // Compara posição atual com anterior e incrementa em caso positivo
         if (v[id] >= v[id - 1]) 
             id++; 
 
         else{ 
 
             (*numTrocas)++;
+            // Troca elementos da posição atual com a anterior
             swap(&v[id], &v[id - 1]); 
             id--;
              
--- src/algoritmos/ordenacao/GnomeSort.h
+++ src/algoritmos/ordenacao/GnomeSort.h
@@ -2,6 +2,14 @@
 #define GNOMESORT_H
 
 // Função de ordenação gnome sort
+/**
+ * @author eliascassis
+ * @param arr vetor de inteiros sem sinal
+ * @param n tamanho do vetor
+ * @param numComparacoes número de comparações
+ * @param numTrocas número de trocas
+ * @return void
+ */
 void gnomeSort(unsigned arr[], unsigned n, unsigned long long *numComparacoes, unsigned long long *numTrocas) ;
 
 #endif // GNOMESORT_H
