0cd2ae86166251a2af24960bcc567568d886d11a
Elias Cyrino <eliascyrino@gmail.com>
2019-10-14 12:28:19 -0300

    hashEncad categorias documentado

--- src2/hashEncadCategorias.cpp
+++ src2/hashEncadCategorias.cpp
@@ -2,40 +2,48 @@
 #include "funcoesAuxiliares.h"
 #include<tuple>
 
-
+// Construtor
 HashEncadCategorias::HashEncadCategorias(unsigned n){
 
+    // Inicializando atributos
     this->n = n;
     this->tabela = new list<string>[this->n];
 
 }
 
+// Função de hash
 unsigned HashEncadCategorias::funcaoHash(string chave){ 
     return (converterStringUnsigned(chave) % this->n); 
 }
 
+// Função que insere item na tabela
 void HashEncadCategorias::insereItem(string chave){
 
     unsigned indice = funcaoHash(chave);
     tabela[indice].push_back(chave);
 }
 
+// Retorna a frequência da chave
 unsigned HashEncadCategorias::getFrequencia(string chave){
 
     return this->tabela[this->funcaoHash(chave)].size();
 
 }
 
+// Retorna categorias associadas a suas respectivas frequências
 list<string>* HashEncadCategorias::gerarVetorFrequencia(){
 
     unsigned tamFreq = 0;
 
+    // Contabiliza posições não vazias
     for(int i = 0; i < this->n; i++)
         if(this->tabela[i].size() != 0)
             tamFreq++;
 
-    list<string> *tabFreq = (new list<string>[tamFreq]);  
+    // Aloca lista para categorias
+    list<string> *tabFreq = new list<string>[tamFreq];
 
+    // Copia posições não vazias para tabela para frequências
     for(int i = 0, j = 0; i < this->n; i++)
         if(this->tabela[i].size() != 0){
 
@@ -44,17 +52,20 @@ list<string>* HashEncadCategorias::gerarVetorFrequencia(){
 
         }
 
+    // Chama o Heap Sort para ordenação decrescente das frequências
     heapSort(tabFreq, tamFreq);
 
+    // Retorna vetor de frequências e de categorias
     return tabFreq;
 
 }
 
+// Exibe a frequência das categorias
 void HashEncadCategorias::exibirFrequencias(unsigned n){
 
     list<string>* freq;
     freq = this->gerarVetorFrequencia();
-    cout << "Frequência das categorias:" << endl;
+    cout << "Frequência das Categorias:" << endl;
     for(int i = 0; i < n; i++)
         cout << i + 1 << " - " << freq[i].front() <<  " - " << freq[i].size() << endl;
     cout << endl;
--- src2/hashEncadCategorias.h
+++ src2/hashEncadCategorias.h
@@ -7,19 +7,20 @@ using namespace std;
 
 class HashEncadCategorias{
 
+    // Atributos de HashEncadCategorias
     private:
         unsigned n; //Número do maior usuário.
-        list<string> *tabela;
+        list<string> *tabela; // Vetor de lista de strings
 
     public:
-        HashEncadCategorias(unsigned n);
-        void insereItem(string chave);
-        unsigned funcaoHash(string chave);
-        unsigned getFrequencia(string chave); // Retorna frequência de dado usuário
+        HashEncadCategorias(unsigned n); // Construtor
+        void insereItem(string chave); // Insere item
+        unsigned funcaoHash(string chave); // Função de hash
+        unsigned getFrequencia(string chave); // Retorna frequência de dada categoria
         void exibirFrequencias(unsigned n); // exibe n frequências
     
     private:
-        list<string>* gerarVetorFrequencia(); 
+        list<string>* gerarVetorFrequencia(); // Gera vetor de frequências
 
 };
 
