e6d200c310bf14bf07e45945cd08ab9ebfeb2a79
Elias Cyrino <eliascyrino@gmail.com>
2019-10-14 18:44:14 -0300

    quickSortMediano documentado

--- src/algoritmos/ordenacao/quickSortMediano.cpp
+++ src/algoritmos/ordenacao/quickSortMediano.cpp
@@ -4,52 +4,62 @@
 
 using namespace std;
 
+// Gera um vetor com k elementos da partição
 unsigned* gerarVetor(unsigned* v, unsigned inicio, unsigned k){
     
-    unsigned *aux = (unsigned*)malloc(k*sizeof(unsigned));
+    unsigned *aux = (unsigned*)malloc(k*sizeof(unsigned)); // aloca vetor de tamanho k
 
+    // Insere k elementos no vetor alocado
     for(unsigned i = 0; i < k; i++, inicio++ )
         aux[i] = v[inicio];
 
-    return aux;
+    return aux; // Retorna vetor com k elementos
 
 }
 
+// Retorna a mediana de k elementos
 unsigned medianadek(unsigned* vetor, unsigned k, unsigned long long* trocas, unsigned long long *comparacoes){
 
-    quickSortRecursivoIds(vetor, 0, k-1, comparacoes, trocas);
+    quickSortRecursivoIds(vetor, 0, k-1, comparacoes, trocas); // ordena os k elementos
 
-    unsigned mediana = k/2;
+    unsigned mediana = vetor[k/2]; // Pega a mediana
 
-    return mediana;
+    return mediana; // Retorna a mediana
 
 }
 
+// quickSort que utiliza como pivo a mediana de k elementos da partição
 void quickSortMediano(unsigned *vetor, unsigned inicio, unsigned fim, unsigned k, unsigned long long *trocas, unsigned long long *comparacoes) {
 
 
+    // Parada da recursão
     if(inicio < fim){
 
+        // Conferindo condição de k para cálculo da mediana
         if (fim - inicio < k) {
-            quickSortRecursivoIds(vetor, inicio, fim, comparacoes, trocas);
+
+            // Chama quickSort recursivo
+            quickSortRecursivoIds(vetor, inicio, fim, comparacoes, trocas); 
 
         }else {
 
-            unsigned *aux = gerarVetor(vetor, inicio, k);
+            unsigned *aux = gerarVetor(vetor, inicio, k); // Gera vetor com k elementos
 
-            unsigned mediana = medianadek(aux, k, trocas, comparacoes);
+            unsigned mediana = medianadek(aux, k, trocas, comparacoes); // Calcula a mediana de k elementos
 
             unsigned copiaInicio = inicio;
+            // Loop para encontrar id da mediana
             while(aux[mediana] != vetor[copiaInicio])
                 copiaInicio++;
 
-            delete[] aux;
+            delete[] aux; // Desaloca vetor auxiliar
 
             mediana = copiaInicio;
 
-            (*trocas)++;
-            swap(&vetor[mediana], &vetor[fim]);
+            (*trocas)++; // Incrementa o número de trocas
+            swap(&vetor[mediana], &vetor[fim]); // Troca a media para a posição final do vetor
 
+            // Chamadas recursivas do quickSortMediano
             unsigned pivo = particaoIds(vetor, inicio, fim, comparacoes, trocas);
             quickSortMediano(vetor, inicio, pivo - 1, k, trocas, comparacoes);
             quickSortMediano(vetor, pivo + 1, fim, k, trocas, comparacoes);
--- src/algoritmos/ordenacao/quickSortMediano.h
+++ src/algoritmos/ordenacao/quickSortMediano.h
@@ -2,10 +2,41 @@
 #define QUICKSORTMEDIANO_H
 #include "../../classes/review.h"
 
+/**
+ * Retorna um vetor de k elementos
+ *
+ * @author caiosdeo
+ * @param v Vetor com quantidade de elementos >= k
+ * @param inicio Tamanho do conjunto
+ * @param k quantidade de elementos
+ * @return aux Vetor com k elementos
+ */
 unsigned* gerarVetor(unsigned* v, unsigned inicio, unsigned k);
 
+/**
+ * Calcula a mediana de k elementos
+ *
+ * @author caiosdeo
+ * @param vetor vetor de inteiros sem sinal
+ * @param k Tamanho do conjunto
+ * @param trocas ponteiro para número de trocas
+ * @param comparacoes ponteiro para número de comparações
+ * @return mediana 
+ */
 unsigned medianadek(unsigned *vetor, unsigned k, unsigned long long *trocas, unsigned long long *comparacoes);
 
+/**
+ * quickSort com mediana de k elementos
+ *
+ * @author caiosdeo
+ * @param vetor Vetor unsigned
+ * @param inicio início da partição
+ * @param fim fim da partição
+ * @param k quantidade de elementos para cálculo da mediana
+ * @param numComparacoes número de comparações
+ * @param numTrocas número de trocas
+ * @return void
+ */
 void quickSortMediano(unsigned *vetor, unsigned inicio, unsigned fim, unsigned k, unsigned long long *trocas, unsigned long long *comparacoes);
 
 #endif // QUICKSORTMEDIANO_H
