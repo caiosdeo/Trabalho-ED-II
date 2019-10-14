c70ec41151782dfde951c666e646509662e8bf81
Elias Cyrino <eliascyrino@gmail.com>
2019-10-14 18:48:15 -0300

    quickSortInsertion documentado

--- src/algoritmos/ordenacao/quickSortInsertion.cpp
+++ src/algoritmos/ordenacao/quickSortInsertion.cpp
@@ -4,14 +4,15 @@
 #include "quickSortRecursivo.h"
 using namespace std;
 
-
+// QuickSort utilizando insertion sort para partições de tamanho <= m
 void insertionQuickSort(unsigned *a, unsigned inicio, unsigned fim, unsigned m, unsigned long long *numComparacoes, unsigned long long *numTrocas)
 {
 
+		// Confere condição para chamada do insertion Sort
 		if(fim - inicio < m)
-			insertionSort(a, fim-inicio, numComparacoes, numTrocas);
+			insertionSort(a, fim-inicio, numComparacoes, numTrocas); // Chama insertion sort
 
 		else
-			quickSortRecursivoIds(a, inicio, fim, numComparacoes, numTrocas);
+			quickSortRecursivoIds(a, inicio, fim, numComparacoes, numTrocas); // Chama quickSort recursivo
 
 }
--- src/algoritmos/ordenacao/quickSortInsertion.h
+++ src/algoritmos/ordenacao/quickSortInsertion.h
@@ -1,3 +1,13 @@
 #include <fstream>
 
+/**
+ * @author PaulaRRP
+ * @param a vetor de inteiros sem sinal
+ * @param inicio início da partição
+ * @param fim fim da partição
+ * @param m tamanho da partição para chamada do insertionSort
+ * @param numComparacoes número de comparações
+ * @param numTrocas número de trocas
+ * @return void
+ */
 void insertionQuickSort(unsigned *a, unsigned inicio, unsigned fim, unsigned m, unsigned long long *numComparacoes, unsigned long long *numTrocas);
