a3cc86759b2befe9b45a243ce8188a85ea2b1a55
Elias Cyrino <eliascyrino@gmail.com>
2019-10-14 18:24:51 -0300

    quickSort documentado

--- src/algoritmos/ordenacao/quickSortRecursivo.cpp
+++ src/algoritmos/ordenacao/quickSortRecursivo.cpp
@@ -18,36 +18,47 @@ void swap(unsigned *i, unsigned *j){
 // Partição para ids
 unsigned particaoIds(unsigned *a, unsigned inicio, unsigned fim, unsigned long long *numComparacoes, unsigned long long *numTrocas){
 
+    // Pega um pivô aleatório
     srand(time(0));
     unsigned pivo = a[rand()%(fim - inicio) + inicio];
+    // Coloca pivo no fim da partição
+    swap(&a[pivo], &a[fim]);
+    (*numTrocas)++; // Incrementa o número de trocas
+    // i recebe decremento do início
     unsigned i = inicio - 1;
 
-    for(unsigned j = inicio; j < fim; j++){
+    // Loop de particionamento
+    for(int j = inicio; j < fim; j++){
 
+        // Troca a posição do elemento caso seja menor ou igual ao pivô
         (*numComparacoes)++;
         if(a[j] <= pivo){
 
             i++;
             swap(&a[i], &a[j]);
-            (*numTrocas)++;
+            (*numTrocas)++; // Incrementa o número de trocas
 
         }
 
     }
 
-    swap(&a[i+1], &a[fim]);
-    (*numTrocas)++;
-    return i+1;
+    swap(&a[i+1], &a[fim]); // Joga o novo pivô para o fim da partição
+    (*numTrocas)++; // Incrementa o número de trocas
+    return i+1; // Atualiza o pivô
 
 }
 
 // quickSortRecursivo para ids
 void quickSortRecursivoIds(unsigned *a, unsigned inicio, unsigned fim, unsigned long long *numComparacoes, unsigned long long *numTrocas){
 
+    // Parada da recursão
     if(inicio < fim){
 
+        // Pivo da partição
         unsigned pivo = particaoIds(a, inicio, fim, numComparacoes, numTrocas);
+        // Chama quickSort para valores menores que pivo
         quickSortRecursivoIds(a, inicio, pivo-1, numComparacoes, numTrocas);
+        // Chama quickSort para valores maiores que pivo
         quickSortRecursivoIds(a, pivo+1, fim, numComparacoes, numTrocas);
 
     }
@@ -70,39 +81,51 @@ void swapEstruturas(Review* a, unsigned i, unsigned j){
 // particao para estruturas
 unsigned particaoEstruturas(Review* a, unsigned inicio, unsigned fim, unsigned long long *numComparacoes, unsigned long long *numTrocas){
 
-    unsigned pivo = a[fim].getId();
-    unsigned i = inicio - 1;
+    // Pega um pivô aleatório
+    srand(time(0));
+    unsigned pivo = a[rand()%(fim - inicio) + inicio].getId();
+    // Coloca pivo no fim da partição
+    swapEstruturas(a, pivo, fim);
+    (*numTrocas)++; // Incrementa o número de trocas
+    // i recebe decremento do início
+    unsigned i = inicio - 1; 
 
-    for(unsigned j = inicio; j < fim; j++){
+    // Loop de particionamento
+    for(int j = inicio; j < fim; j++){
 
+        // Troca a posição do elemento caso seja menor ou igual ao pivô
         (*numComparacoes)++;
         if(a[j].getId() <= pivo){
 
             i++;
             swapEstruturas(a, i, j);
-            (*numTrocas)++;
+            (*numTrocas)++; // Incrementa o número de trocas
 
         }
 
     }
-
+    // Joga o novo pivô para o fim da partição
     swapEstruturas(a, i+1, fim);
-    (*numTrocas)++;
-    return i+1;
+    (*numTrocas)++; // Incrementa o número de trocas
+    return i+1; // Atualiza o pivô
 
 }
 
 // quickSortRecursivo para estruturas
 void quickSortRecursivoEstruturas(Review* a, unsigned inicio, unsigned fim, unsigned long long *numComparacoes, unsigned long long *numTrocas){
 
+    // Parada da recursão
     if(inicio < fim){
 
+        // Pivo da partição
         unsigned pivo = particaoEstruturas(a, inicio, fim, numComparacoes, numTrocas);
-        if(pivo != 0)
-            quickSortRecursivoEstruturas(a, inicio, pivo-1, numComparacoes, numTrocas);
-        if(pivo != fim)
-            quickSortRecursivoEstruturas(a, pivo+1, fim, numComparacoes, numTrocas);
+        // Chama quickSort para valores menores que pivo
+        quickSortRecursivoEstruturas(a, inicio, pivo-1, numComparacoes, numTrocas);
+        // Chama quickSort para valores maiores que pivo
+        quickSortRecursivoEstruturas(a, pivo+1, fim, numComparacoes, numTrocas);
 
     }
+
+    return;
 
 }
