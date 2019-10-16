bcd2fb7511975970add8d78145ae005331124d43
Elias Cyrino <eliascyrino@gmail.com>
2019-10-16 15:09:20 -0300

    arquivos noB adicionados

---
+++ src3/arvoreB.h
@@ -0,0 +1,7 @@
+#ifndef ARVOREB_H_INCLUDED
+#define ARVOREB_H_INCLUDED
+#include "noB.h"
+
+
+
+#endif // ARVOREB_H_INCLUDED
---
+++ src3/noB.h
@@ -0,0 +1,6 @@
+#ifndef NOB_H_INCLUDED
+#define NOB_H_INCLUDED
+
+
+
+#endif // NO_H_INCLUDED
