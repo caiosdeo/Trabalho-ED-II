640dd8d500517ab6d9f7477553732e1f8aca8bdd
AndreCasarim <andrleite@ymail.com>
2019-10-20 23:38:15 -0300

    comentarios adicionados

--- src3/nodoRB.cpp
+++ src3/nodoRB.cpp
@@ -3,76 +3,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#define RED 1
-#define BLACK 0
+#define RUB 1 //define para cor vermelha/rubro
+#define PRET 0 // define para cor preta/negro
 
 using namespace std;
 
-nodoRB::nodoRB(int id, string user)
+//Construtor
+nodoRB::nodoRB(int chave, string user)
 {
-    this->id = id;
+    //inicializamdo variaveis chave(id), user e cor
+    this->chave = chave;
     this->user = user;
-    this->dir = NULL;
+    this->cor = RUB;
+    //inicializando vairaveis esq, dir e pai como vazio
     this->esq = NULL;
+    this->dir = NULL;
     this->pai = NULL;
-    this->cor = RED;
 }
-
+//funcao que retorna variavel Id
 int nodoRB::getId()
 {
-    return this->id;
+    return this->chave;
 }
-
+//funcao que retorna variavel User
 string nodoRB::getUser()
 {
     return this->user;
 }
-
+//funcao que retorna variavel Cor
 bool nodoRB::getCor()
 {
     return this->cor;
 }
-
+//funcao que retorna ponteiro do filho a esquerda
 nodoRB* nodoRB::getEsq()
 {
     return this->esq;
 }
-
+//funcao que retorna ponteiro do filho a direita
 nodoRB* nodoRB::getDir()
 {
     return this->dir;
 }
-
+//funcao que retorna ponteiro do pai
 nodoRB* nodoRB::getPai()
 {
     return this->pai;
 }
-
-void nodoRB::setId(int id)
-{
-    this->id = id;
-}
-
-void nodoRB::setUser(string user)
-{
-    this->user = user;
-}
-
+//funcao para colocar cor
 void nodoRB::setCor(bool cor)
 {
     this->cor = cor;
 }
-
+//funcao para colocar filho a esquerda
 void nodoRB::setEsq(nodoRB *esq)
 {
     this->esq = esq;
 }
-
+//funcao para colocar filho a direita
 void nodoRB::setDir(nodoRB *dir)
 {
     this->dir = dir;
 }
-
+//funcao para colocar pai
 void nodoRB::setPai(nodoRB *pai)
 {
     this->pai = pai;
--- src3/nodoRB.h
+++ src3/nodoRB.h
@@ -10,28 +10,29 @@ using namespace std;
 class nodoRB
 {
     public:
-        nodoRB(int id, string user);
-        ~nodoRB(){};
+        nodoRB(int chave, string user); //construtor
 
-        int getId();
-        string getUser();
-        bool getCor();
-        nodoRB* getEsq();
-        nodoRB* getDir();
-        nodoRB* getPai();
+        int getChave(); //função que retorna variável chave
+        string getUser(); //função que retorna variável User
+        bool getCor(); //função que retorna variável Cor
+        nodoRB* getEsq(); //função que retorna ponteiro do filho a esquerda
+        nodoRB* getDir(); //função que retorna ponteiro do filho a direita
+        nodoRB* getPai(); //função que retorna ponteiro do pai
 
-        void setId(int id);
-        void setUser(string user);
-        void setCor(bool cor);
-        void setEsq(nodoRB *esq);
-        void setDir(nodoRB *dir);
-        void setPai(nodoRB *pai);
+        void setCor(bool cor); //função para colocar cor
+        void setEsq(nodoRB *esq); //função para colocar filho a esquerda
+        void setDir(nodoRB *dir); //função para colocar filho a direita
+        void setPai(nodoRB *pai); //função para colocar pai
 
     private:
-        int id;
-        string user;
-        bool cor;
-        nodoRB *esq, *dir, *pai;
+        int chave; //{variavel para chave, será usada para comparações e armazenar Id
+        string user; //vairiável para User                                           } Variáveis de informação
+
+        bool cor; //variável que define a cor do nodo
+        nodoRB *esq, *dir, *pai; //ponteiros para os dois filhos, esquerda e direita, e pai
+
+    // Tornando os atributos do nodo acessíveis para as funções da classe arvoreRB
+    friend class arvoreRB;
 };
 
 #endif // NODORB_H
