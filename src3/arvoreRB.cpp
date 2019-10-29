c64ec7e1e93b7da4e0b92b714f58196437fcb45b
AndreCasarim <andrleite@ymail.com>
2019-10-28 21:48:06 -0300

    Funcoes de balanciamento para insercao implementados, corrigidos funcoes de rotacao

--- src3/arvoreRB.cpp
+++ src3/arvoreRB.cpp
@@ -2,7 +2,7 @@
 #include "nodoRB.h"
 #include <iostream>
 #include <string>
-#define RUB 1
+#define VER 1
 #define PRET 0
 
 using namespace std;
@@ -32,7 +32,6 @@ arvoreRB::~arvoreRB()
 void arvoreRB::insereChave(int chave, string user)
 {
     nodoRB *novo = new nodoRB(chave, user); //aloca novo nodo
-
     //se árvore vazia
     if(this->raiz == NULL)
         {
@@ -68,32 +67,125 @@ void arvoreRB::insereChave(int chave, string user)
         }
     }
     //funções para verificar e fazer tanto balanceamento quanto recolorização
-    if(novo != this->raiz && novo->getCor() != PRET)
-    {
+    balancearInsercao(novo);
+}
 
+void arvoreRB::balancearInsercao(nodoRB *x)
+{
+    if(x != this->raiz)
+    {
+        nodoRB *auxPai = x->getPai();
+        if(auxPai != NULL && auxPai->getCor() != PRET)
+        {
+            if(!recolorir(x))
+            {
+                auxBalancear(x, auxPai);
+            }
+        }
     }
+}
+
+bool arvoreRB::recolorir(nodoRB *x)
+{
+    nodoRB *auxPai = x->getPai();
+    if(this->raiz->getCor() == VER) this->raiz->setCor(PRET);
+    nodoRB *avo = auxPai->getPai(), *tio;
+    if(avo->getEsq() != auxPai)
+        tio = avo->getEsq();
     else
+        tio = avo->getDir();
+    if(tio != NULL && tio->getCor() != PRET)
     {
-        if(novo == RUB)
-            //trocar cor
+        auxRecolorir(x);
+        return true;
     }
+    return false;
+}
 
+void arvoreRB::auxRecolorir(nodoRB *x)
+{
+    nodoRB *auxPai = x->getPai(), *auxAvo, *auxTio;
+    if(auxPai != NULL)
+    {
+        auxPai->setCor(PRET);
+        auxAvo = auxPai->getPai();
+        if(auxAvo != NULL)
+        {
+            auxAvo->setCor(VER);
+            if(auxAvo->getEsq() != auxPai)
+                auxTio = auxAvo->getEsq();
+            else
+                auxTio = auxAvo->getDir();
+            if(auxTio != NULL)
+                auxTio->setCor(PRET);
+            auxRecolorir(auxAvo);
+        }
+    }
+}
+
+void arvoreRB::auxBalancear(nodoRB *x, nodoRB *auxPai)
+{
+    nodoRB *auxAvo = auxPai->getPai();
+    if(auxAvo->getDir() == auxPai && auxPai->getDir() == x)
+    {
+        rotacaoEsq(auxPai, auxAvo);
+        auxPai->setCor(PRET);
+        auxAvo->setCor(VER);
+    }
+    if(auxAvo->getEsq() == auxPai && auxPai->getEsq() == x)
+    {
+        rotacaoDir(auxPai, auxAvo);
+        auxPai->setCor(PRET);
+        auxAvo->setCor(VER);
+    }
+    if(auxAvo->getEsq() == auxPai && auxPai->getDir() == x)
+    {
+        rotacaoEsq(x, auxPai);
+        rotacaoDir(x, auxAvo);
+        x->setCor(PRET);
+        x->getDir()->setCor(VER);
+    }
+    if(auxAvo->getDir() == auxPai && auxPai->getEsq() == x)
+    {
+        rotacaoDir(x, auxPai);
+        rotacaoEsq(x, auxAvo);
+        x->setCor(PRET);
+        auxAvo->setCor(VER);
+    }
 }
 //Função de rotação à esquerda
-nodoRB* arvoreRB::rotacaoEsq(nodoRB *aux)
+void arvoreRB::rotacaoEsq(nodoRB *x, nodoRB *pai)
 {
-    nodoRB *pai = aux->getPai();
-    aux->setPai(pai->getPai());
-    pai->setPai(aux);
-    aux->setDir(pai);
-    return aux;
+    nodoRB *auxAvo = pai->getPai();
+    if(auxAvo != NULL)
+    {
+        if(auxAvo->getEsq() == pai)
+            auxAvo->setEsq(x);
+        else
+            auxAvo->setDir(x);
+        if(auxAvo == this->raiz)
+            this->raiz = x;
+    }
+    x->setPai(auxAvo);
+    pai->setPai(x);
+    pai->setDir(x->getEsq());
+    x->setEsq(pai);
 }
 //Função de rotação à direita
-nodoRB* rotacaoDir(nodoRB *aux)
+void arvoreRB::rotacaoDir(nodoRB *x, nodoRB *pai)
 {
-    nodoRB *pai = aux->getPai();
-    aux->setPai(pai->getPai());
-    pai->setPai(aux);
-    aux->setEsq(pai);
-    return aux;
+    nodoRB *auxAvo = pai->getPai();
+    if(auxAvo != NULL)
+    {
+        if(auxAvo->getEsq() == pai)
+            auxAvo->setEsq(x);
+        else
+            auxAvo->setDir(x);
+        if(auxAvo == this->raiz)
+            this->raiz = x;
+    }
+    x->setPai(auxAvo);
+    pai->setPai(x);
+    pai->setEsq(x->getDir());
+    x->setDir(pai);
 }
--- src3/arvoreRB.h
+++ src3/arvoreRB.h
@@ -13,12 +13,18 @@ class arvoreRB
         void insereChave(int chave, string user);
         void removeChave(int chave);
         void imprime();
+        int getAltura();
+        bool recolorir(nodoRB *x);
 
     private:
         nodoRB *raiz;
         nodoRB* libera(nodoRB *aux);
-        nodoRB* rotacaoEsq(nodoRB *aux);
-        nodoRB* rotacaoDir(nodoRB *aux);
+        void rotacaoEsq(nodoRB *pai, nodoRB *avo);
+        void rotacaoDir(nodoRB *pai, nodoRB *avo);
+        void balancearInsercao(nodoRB *x);
+        void auxBalancear(nodoRB *x, nodoRB *auxPai);
+        void auxRecolorir(nodoRB *x);
+
 };
 
 #endif // ARVORERB_H
